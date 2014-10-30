eb29eb756720c1a556828d9bf3f2f9543a87e2e1
Viproz <viproz12@gmail.com>
2014-10-30 13:55:10 +0100

    average

--- MapGenerator/MapGenerator.cs
+++ MapGenerator/MapGenerator.cs
@@ -8,6 +8,9 @@ namespace MapGenerator
 {
 	public class MapGenerator : CSharpPlugin
 	{
+		float[,] heightmap;
+		int heightFactor = 35000;
+
 		public void On_PluginInit()
 		{
 			Logger.LogError("putain ca marche");
@@ -19,31 +22,57 @@ namespace MapGenerator
 		{
 			TerrainGenerator terrainGen = SingletonComponent<TerrainGenerator>.Instance;
 
-			float[,] heightmap = (float[,])typeof(TerrainGenerator).GetField("heightmap", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(terrainGen);
+			heightmap = (float[,])typeof(TerrainGenerator).GetField("heightmap", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(terrainGen);
 			Logger.Log(heightmap.GetLength(0).ToString() + ":" + heightmap.GetLength(1).ToString());
 
 			//Color32[,] colors = ((TerrainMath)typeof(TerrainGenerator).GetField("terrainMath", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(terrainGen)).colormap;
 
 			//Logger.LogError(colors.GetLength(0).ToString() + colors.GetLength(1).ToString());
 
-			Bitmap image = new Bitmap(2048, 2048);
+			Bitmap image = new Bitmap(2049, 2049);
 			for(int i = 0 ; i < 2049 ; i++) {
-				if (i == 2048)
-					continue;
 				for(int j = 0 ; j < 2049 ; j++) {
-					if (j == 2048)
-						continue;
-					if (i == 1500 && j % 100 == 0)
-						Logger.Log(Math.Min(Math.Abs(heightmap[i, j] - heightmap[i, j + 1]) * 35000, 255).ToString());
-					if(heightmap[i, j] < 0.5)
+					if (heightmap[i, j] < 0.5) {
 						image.SetPixel(i, j, Color.FromArgb(0, 20, 185));
-					else
-						//image.SetPixel(i, j, Color.FromArgb((int)((heightmap[i, j] - 0.5) * 510), 255 - (int)((heightmap[i, j] - 0.5) * 510), 20));
-						image.SetPixel(i, j, Color.FromArgb((int)(Math.Min(Math.Abs(heightmap[i, j] - heightmap[i, j + 1]) * 35000, 255)), 10, 20));
+						continue;
+					}
+
+					//image.SetPixel(i, j, Color.FromArgb((int)((heightmap[i, j] - 0.5) * 510), 255 - (int)((heightmap[i, j] - 0.5) * 510), 20));
+					image.SetPixel(i, j, Color.FromArgb(Math.Min(getDiffLevel(i, j), 255), 10, 20));
 					//image.SetPixel(i, j, System.Drawing.Color.FromArgb(colors[i,j].r, colors[i,j].g, colors[i,j].b));
 				}
 			}
-			image.Save(Plugin.ValidateRelativePath("img.png"));
+			image.Save(Plugin.ValidateRelativePath("imgmoy.png"));
+		}
+
+		public int getDiffLevel(int x, int y)
+		{
+			int values = 0;
+			float tmp = 0;
+			float pointHeight = heightmap[x, y];
+			if(x != 0) {
+				tmp += Math.Abs(heightmap[x - 1, y] - pointHeight) * (float)heightFactor;
+				values++;
+			}
+			if(x != 2048) {
+				tmp += Math.Abs(heightmap[x + 1, y] - pointHeight) * (float)heightFactor;
+				values++;
+			}
+			if(y != 0) {
+				tmp += Math.Abs(heightmap[x, y - 1] - pointHeight) * (float)heightFactor;
+				values++;
+			}
+			if(y != 2048) {
+				tmp += Math.Abs(heightmap[x, y + 1] - pointHeight) * (float)heightFactor;
+				values++;
+			}
+			if(x == 1000)
+				Logger.Log(tmp.ToString());
+			int result = (int)(tmp / (float)values);
+			if(x == 1000)
+				Logger.Log(result.ToString());
+
+			return result;
 		}
 	}
 }
